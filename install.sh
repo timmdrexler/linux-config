cbfa56787092ee05529c4366607298c400a6f463
Timm Drexler <timm.drexler@gmail.com>
2016-12-16 10:12:18 -0500

    Updated install.sh and removed old themes

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-rsync -av --delete ./icons/ ~/.icons/
-rsync -av --delete ./themes/ ~/.themes/
+#rsync -av --delete ./icons/ ~/.icons/
+#rsync -av --delete ./themes/ ~/.themes/
+
+cp -R ./icons/* /usr/share/icons
+cp -R ./themes/* /usr/share/themes
 
