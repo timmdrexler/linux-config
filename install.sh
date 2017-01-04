32f72e47938815a1b37720211be92c061f84fb80
Timm Drexler <timm.drexler@gmail.com>
2017-01-04 12:42:13 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@
 cp -R ./icons/* /usr/share/icons
 #cp -R ./themes/* /usr/share/themes
 
-apt install openbox lxappearance pcmanfm gtk-chtheme lxterminal obmenu firefox arc-theme xinit
+apt install openbox lxappearance pcmanfm gtk-chtheme lxterminal obmenu firefox arc-theme xinit x11-xserver-utils
