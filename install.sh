a5ad4d4d7392753aa0535cdd81759298ffaac93d
tdrexler <timm.drexler@gmail.com>
2017-01-13 13:37:49 -0500

    Tweaked install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo "Install base Xorg with OpenBox?" 
 select yn in "Yes" "No"; do
 	case $yn in
-		Yes ) apt install openbox openbox-menu tint2 gsimplecal volumeicon-alsa nitrogen compton lxappearance feh pcmanfm lxterminal obmenu xinit x11-xserver-utils htop firefox chromium-browser; XINSTALL="YES"; break;;
+		Yes ) apt install openbox openbox-menu tint2 gsimplecal mousepad volumeicon-alsa nitrogen compton lxappearance feh pcmanfm lxterminal obmenu xinit x11-xserver-utils htop firefox chromium-browser; XINSTALL="YES"; break;;
 		No ) echo "Xorg skipped"; XINSTALL="NO" break;;
 	esac
 done
@@ -28,7 +28,7 @@ if [ "$XINSTALL" = "YES" ]; then
 	#done
 
 	#Copy themes and setup GUI config files
-	mkdir ~/.config; mkdir ~/.icons; mkdir ~/.themes; mkdir ~/.config/tint2; mkdir ~/.config/nitrogen
+	mkdir ~/.config; mkdir ~/.icons; mkdir ~/.themes; mkdir ~/.config/tint2; mkdir ~/.config/nitrogen; mkdir ~/Pictures
 	cp -r ./config/openbox ~/.config
 	cp ./config/background.png ~/Pictures
 	cp -r ./icons/* ~/.icons
@@ -38,19 +38,23 @@ if [ "$XINSTALL" = "YES" ]; then
 
 	#Fix permissions
 	#chmod 700 ~/.config; chown $USER:$USER ~/.config
-	find ~/.config -exec chown $USER:$USER {} +
+	find ~/.config -exec chown "$SUDO_USER":"$SUDO_USER" {} +
 	find ~/.config -type d -exec chmod 700 {} +
 	find ~/.config -type f -exec chmod 600 {} +
 
 	#chmod 700 ~/.themes; chown $USER:$USER ~/.themes
-	find ~/.themes -exec chown $USER:$USER {} +
+	find ~/.themes -exec chown "$SUDO_USER":"$SUDO_USER" {} +
 	find ~/.themes -type d -exec chmod 700 {} +
 	find ~/.themes -type f -exec chmod 600 {} +
 
 	#chmod 700 ~/.icons; chown $USER:$USER ~/.icons
-	find ~/.icons -exec chown $USER:$USER {} +
+	find ~/.icons -exec chown "$SUDO_USER":"$SUDO_USER" {} +
 	find ~/.icons -type d -exec chmod 700 {} +
 	find ~/.icons -type f -exec chmod 600 {} +
+
+	find ~/Pictures -exec chown "$SUDO_USER":"$SUDO_USER" {} +
+	find ~/Pictures -type d -exec chmod 755 {} +
+	find ~/Pictures -type f -exec chmod 664 {} +
 fi
 
 #=======================================================
